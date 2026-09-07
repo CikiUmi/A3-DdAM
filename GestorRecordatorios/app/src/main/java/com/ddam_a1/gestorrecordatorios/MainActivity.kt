57f96e45ba6cd54a0237dd6ff44c497dd9d1e09d
CikiUmi <escueladhh@gmail.com>
2026-09-07 11:59:49 -0600

    feat(library): se agregó hilt + implementación

--- GestorRecordatorios/app/src/main/java/com/ddam_a1/gestorrecordatorios/MainActivity.kt
+++ GestorRecordatorios/app/src/main/java/com/ddam_a1/gestorrecordatorios/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.ddam_a1.gestorrecordatorios.ui.navigation.RecordatoriosNavHost
 import com.ddam_a1.gestorrecordatorios.ui.theme.GestorRecordatoriosTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- GestorRecordatorios/app/src/main/java/com/ddam_a1/gestorrecordatorios/ui/navigation/Navegacion.kt
+++ GestorRecordatorios/app/src/main/java/com/ddam_a1/gestorrecordatorios/ui/navigation/Navegacion.kt
@@ -1,16 +1,21 @@
 package com.ddam_a1.gestorrecordatorios.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.ddam_a1.gestorrecordatorios.ui.screens.Editar
 import com.ddam_a1.gestorrecordatorios.ui.screens.Main
 import com.ddam_a1.gestorrecordatorios.ui.screens.Papelera
+import com.ddam_a1.gestorrecordatorios.viewmodel.RecordatoriosViewModel
 
 @Composable
-fun RecordatoriosNavHost() {
+fun RecordatoriosNavHost(modifier: Modifier = Modifier) {
+
     val navController = rememberNavController()
+    val vm: RecordatoriosViewModel = hiltViewModel()
 
     NavHost(
         navController = navController,
